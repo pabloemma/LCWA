d68705c224f835dba2695dc2cdaf56f95ed9c323
klein <pabloemma@gmail.com>
2020-02-28 16:33:28 -0700

    updated file

--- scripts/run_cleanup.sh
+++ scripts/run_cleanup.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-sudo -u pi -H sh -c "find /home/pi/speedtest -type f -name '*.csv' -mtime +2 -exec rm {} \; &"
+sudo -u pi -H sh -c "find /home/pi/speedfiles -type f -name '*.csv' -mtime +2 -exec rm {} \; &"
 
