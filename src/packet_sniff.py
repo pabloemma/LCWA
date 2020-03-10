545173041c521e7576bccb8974b6309ca0adad02
andi klein <pabloemma@gmail.com>
2020-03-10 17:23:11 -0600

    start writing sniffer

---
+++ src/packet_sniff.py
@@ -0,0 +1,31 @@
+
+
+'''
+Created on Feb 8, 2020
+
+@author: klein
+
+
+This class is based on a packet sniffer code from
+https://www.binarytides.com/python-packet-sniffer-code-linux/
+
+The plan is to use it with the test_speed to debug LCWA speedissues
+'''
+
+import socket
+
+
+class PacketSniff():
+    
+    
+    def __init__(self):
+        
+    #establish a socket
+        self.so = s =socket.socket( socket.AF_INET , socket.SOCK_RAW ,  socket.IPPROTO_TCP) 
+    # receive a packet
+        while True:
+            print s.recvfrom(65565)
+
+if __name__ == '__main__':
+    
+    PS=PacketSniff()
