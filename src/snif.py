ab0ab13271daade59b74cc27d70ba7f11f6fcb67
andi klein <pabloemma@gmail.com>
2020-03-16 16:42:54 -0600

    changed socket command

--- src/snif.py
+++ src/snif.py
@@ -6,7 +6,7 @@ from struct import *
 
 #create an INET, STREAMing socket
 try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+	s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 except socket.error , msg:
 	print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 	sys.exit()
