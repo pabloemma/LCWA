f2721f4ec6b09d75eb80e7e61c5cdb42d48c0c5b
andi klein <pabloemma@gmail.com>
2020-04-16 13:40:11 -0600

    Working version

--- src/PlotClass.py
+++ src/PlotClass.py
@@ -15,11 +15,11 @@ import csv
 import time
 import sys
 import os.path
-#import dropbox
+import dropbox
 
 
 
-class MyClass(object):
+class MyPlot(object):
     '''
     classdocs
     '''
@@ -38,14 +38,17 @@ class MyClass(object):
         self.MyPythonVersion()
         
         #now check if file is available, if not we exit
+        
         file = path+'/'+filename
         
         if(self.IsFile(file)):
             self.InputFile = file
         if(self.IsFile(token)):
             self.TokenFile = token
+        
+        self.dropbox_name = filename.replace('csv','pdf')
          
-            
+        self.path = path    
         
         
 
@@ -68,10 +71,13 @@ class MyClass(object):
     def ReadFile(self):
         """ reads the csv file from the speedfile directory"""
         
-        self.temp_file = open(self.path+'/temp.txt',"w")
-        counter = 0
-        for line in open(self.file, 'r'):
         
+        
+        
+        self.temp_file = open(self.path+'/temp.txt',"r+")
+        counter = 0
+        for line in open(self.InputFile, 'r'):
+            print(line)
             a = line.split(',')
             if(len(a)< 9):
                 print ('problem',a)
@@ -79,16 +85,19 @@ class MyClass(object):
             else:
                 self.temp_file.write(line)
 
-            counter++1
+            counter = counter+1
+            print('counter',counter)
             
 
-        self.temp_file.close()
+        #self.temp_file.close()
         
     def ReadTestData(self):
         """
         Reads the results with Matplotlib
         """
         
+        self.ReadFile()
+        self.temp_file.seek(0)
 
         if(self.MyPythonVersion):
             x1,y1,y2 = np.loadtxt(self.temp_file, delimiter=',',
@@ -101,6 +110,7 @@ class MyClass(object):
         self.x1 = x1
         self.y1 = y1
         self.y2 = y2
+        self.PlotTestData(x1, y1, y2)
         
     
     def PlotTestData(self,x1,y1,y2):
@@ -110,9 +120,9 @@ class MyClass(object):
         np.set_printoptions(precision=2)
         fig=plt.figure() 
         ax=fig.add_subplot(1,1,1)
-        ax.text(.1,.36,'Average $\mu$ and Standard deviation $\sigma$',weight='bold',transform=ax.transAxes,fontsize=13)
-        ax.text(.1,.23,r'$\mu_{up}     = $'+str(np.around(np.mean(y2),2))+' '+'[Mb/s]'+r'   $\sigma_{up} =     $'+str(np.around(np.std(y2),2)),transform=ax.transAxes,fontsize=12)
-        ax.text(.1,.3,r'$\mu_{down} = $'+str(np.around(np.mean(y1),2))+' '+'[Mb/s]'+r'   $\sigma_{down} = $'+str(np.around(np.std(y1),2)),transform=ax.transAxes,fontsize=12)
+        #ax.text(.1,.36,'Average $\mu$ and Standard deviation $\sigma$',weight='bold',transform=ax.transAxes,fontsize=13)
+        #ax.text(.1,.23,r'$\mu_{up}     = $'+str(np.around(np.mean(y2),2))+' '+'[Mb/s]'+r'   $\sigma_{up} =     $'+str(np.around(np.std(y2),2)),transform=ax.transAxes,fontsize=12)
+        #ax.text(.1,.3,r'$\mu_{down} = $'+str(np.around(np.mean(y1),2))+' '+'[Mb/s]'+r'   $\sigma_{down} = $'+str(np.around(np.std(y1),2)),transform=ax.transAxes,fontsize=12)
 
         plt.plot_date(x1,y1,'bs',label='\n blue DOWN ')
         plt.plot_date(x1,y2,'g^',label=' green UP')
@@ -124,16 +134,16 @@ class MyClass(object):
         plt.xlabel('Time')
         plt.ylabel('Speed in Mbs')
 
-        plt.title('Speedtest LCWA using '+self.file)
+        plt.title('Speedtest LCWA using '+self.InputFile)
     
         plt.legend(facecolor='ivory',loc="lower right",shadow=True, fancybox=True)
         plt.ylim(0.,24.) # set yaxis limit
         plt.xticks(rotation='vertical')
         plt.tight_layout()
-        file2 = self.file.replace('csv','pdf')
+        self.output = self.InputFile.replace('csv','pdf')
 
-        print (file2)
-        fig.savefig(file2, bbox_inches='tight')
+        print (self.output)
+        fig.savefig(self.output, bbox_inches='tight')
         plt.show()
 
 
@@ -163,7 +173,45 @@ class MyClass(object):
             print('no file:   ' , filename)
             sys.exit(0)
 
-       
+    def ConnectDropbox(self):
+        """
+        here we establish connection to the dropbox account
+        """
+        #f=open(self.keyfile,"r")
+        #self.key =f.readline() #key for encryption
+        #self.key = pad(self.key,16)
+        #f.close()
+
+        f=open(self.TokenFile,"r")
+        self.data =f.readline() #key for encryption
+        
+
+         
+         
+         
+         #connect to dropbox
+        self.dbx=dropbox.Dropbox(self.data.strip('\n'))
+
+        self.myaccount = self.dbx.users_get_current_account()
+        print('***************************dropbox*******************\n\n\n')
+        print( self.myaccount.name.surname , self.myaccount.name.given_name)
+        print (self.myaccount.email)
+        print('\n\n ***************************dropbox*******************\n')
+
+    
+    
+    
+    def PushFileDropbox(self):  
+        f =open(self.output,"rb")
+
+        self.dbx.files_upload(f.read(),'/LCWA/'+self.dropbox_name,mode=dropbox.files.WriteMode('overwrite', None))
+
        
 if __name__ == '__main__':
-    pass 
+    path = '/Users/klein/speedfiles'
+    file = 'Pand_2020-03-02speedfile.csv'
+    token ='/Users/klein/git/speedtest/LCWA/src/LCWA_d.txt'
+    MP = MyPlot(path,file,token)
+    MP.ReadTestData()
+    MP.ConnectDropbox()
+    MP.PushFileDropbox()
