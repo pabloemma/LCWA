c9ce0dbf463adbcf8fcdb8db6eb651b98dcd056b
Andi Klein <pabloemma@gmail.com>
2020-04-19 07:06:17 -0600

    run scripts now point to test_speed1_3.py

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 
 
-if [ $(ps -efa | grep -v grep | grep test_speed1.py -c) -gt 0 ] ;
+if [ $(ps -efa | grep -v grep | grep test_speed1_3.py -c) -gt 0 ] ;
 then
     echo "Process running ...";
 else
 
-python /home/pi/git/speedtest/src/test_speed1.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002 &
+python3 /home/pi/git/speedtest/src/test_speed1_3.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002 &
 
 echo "Starting the process";
 fi;
