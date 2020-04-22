1ffef56bdea1648f7fbf8c44ffd7085c7f0df517
andi klein <pabloemma@gmail.com>
2020-04-22 10:30:54 -0600

    intermediate push

--- src/PlotAll.py
+++ src/PlotAll.py
@@ -12,6 +12,8 @@ import dropbox
 import datetime
 import numpy as np
 from pathlib import Path # this is python 3
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
 
 
 
@@ -70,6 +72,17 @@ class PlotAll(object):
         #First make the part of the file which is depending on the date
         MyFileName = self.GetCurrentFileName()
         
+        #next block determines how many graphs we will do
+        graph_count = 0
+
+        for k in range(len(self.DirList)):
+            temp = '/LCWA/'+self.DirList[k]+'/'+self.DirList[k]+MyFileName # file on dropbox
+            if self.DropFileExists(temp):
+                graph_count = graph_count+1
+
+        print ('we have ',graph_count,'  plots')
+        
+        
         #Here starts the loop
         for k in range(len(self.DirList)):
             temp = '/LCWA/'+self.DirList[k]+'/'+self.DirList[k]+MyFileName # file on dropbox
@@ -79,7 +92,11 @@ class PlotAll(object):
                 print ("getting file " ,temp, '   and storing it at : ',temp_local)
                 
                 filename = self.dbx.files_download_to_file(temp_local,temp)
+                
+                # Read the local file
+                self.ReadFile(temp_local)
 
+                self.ReadTestData()
                 
 
 
@@ -104,11 +121,13 @@ class PlotAll(object):
         
         
         
-
-        self.temp_file = open(self.SetTempDirectory()+'/temp.txt','w')
+        self.temp_name = self.SetTempDirectory()+'/temp.txt'
+        self.temp_file = open(self.temp_name,'w')
         counter = 0
         for line in open(InputFile, 'r'):
             a = line.split(',')
+            if(counter==0):
+                self.MyIP =a[0].strip('day')
             if(len(a)< 9):
                 print ('problem',a)
                 print ('ignore data point at line ',counter+1)
@@ -120,31 +139,22 @@ class PlotAll(object):
 
         self.temp_file.close()
         
-    def ReadTestData(self,legend):
+    def ReadTestData(self):
         """
         Reads the results with Matplotlib
         """
         
-        self.ReadFile()
         
-        self.legend = legend #legend is a dictionary'
+        #self.legend = legend #legend is a dictionary'
         
-        if(self.MyPythonVersion):
            
-            x1,y1,y2 = np.loadtxt(self.temp_name, delimiter=',',
+        x1,y1,y2 = np.loadtxt(self.temp_name, delimiter=',',
                    unpack=True,usecols=(1,7,8),
                    converters={ 1: self.MyTime},skiprows = 1)
             
-        else:
-          
-                  
-            x1,y1,y2 = np.loadtxt(self.temp_name, delimiter=',',
-                   unpack=True,usecols=(1,7,8),
-                   converters={ 1: md.strpdate2num('%H:%M:%S')},skiprows=1)
         self.x1 = x1
         self.y1 = y1
         self.y2 = y2
-        self.PlotTestData(x1, y1, y2)
 
     
     def SetTempDirectory(self):
@@ -163,7 +173,67 @@ class PlotAll(object):
             print(" Creating  ",MyTempDir)
             return MyTempDir
             
+    def MyTime(self,b):
+        """ conversion routine for time to be used in Matplotlib"""
+
         
+        s=b.decode('ascii')
+        
+        a =md.date2num(datetime.datetime.strptime(s,'%H:%M:%S'))    
+        
+        return a
+    
+    
+    def PlotSetup(self,graph_count):
+        """
+        Creates the plotting environment
+        """
+        
+        
+    
+    
+        
+    def PlotTestData(self,x1,y1,y2):
+        """
+        Plots the tests
+        """
+        np.set_printoptions(precision=2)
+        fig=plt.figure() 
+        ax=fig.add_subplot(1,1,1)
+        
+        #Add Ip address
+        
+        
+        #ax.text(.1,.36,'Average $\mu$ and Standard deviation $\sigma$',weight='bold',transform=ax.transAxes,fontsize=13)
+        #ax.text(.1,.23,r'$\mu_{up}     = $'+str(np.around(np.mean(y2),2))+' '+'[Mb/s]'+r'   $\sigma_{up} =     $'+str(np.around(np.std(y2),2)),transform=ax.transAxes,fontsize=12)
+        #ax.text(.1,.3,r'$\mu_{down} = $'+str(np.around(np.mean(y1),2))+' '+'[Mb/s]'+r'   $\sigma_{down} = $'+str(np.around(np.std(y1),2)),transform=ax.transAxes,fontsize=12)
+
+        #add legend
+        print(self.legend)
+        ax.text(.05,.95,'MyIP = '+self.DigIP(),weight='bold',transform=ax.transAxes,fontsize=11)
+
+        plt.plot_date(x1,y1,'bs',label='\n blue DOWN ')
+        plt.plot_date(x1,y2,'g^',label=' green UP')
+        #plt.text(1.,1.,r' $\sigma = .1$')
+        plt.grid(True)
+
+        ax.xaxis.set_major_locator(md.MinuteLocator(interval=60))
+        ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
+        plt.xlabel('Time')
+        plt.ylabel('Speed in Mbs')
+
+        plt.title('Speedtest LCWA '+self.InputFile)
+    
+        plt.legend(facecolor='ivory',loc="upper right",shadow=True, fancybox=True)
+        plt.ylim(0.,24.) # set yaxis limit
+        plt.xticks(rotation='vertical')
+        plt.tight_layout()
+        print('input file',self.InputFile)
+
+        print (self.output)
+        fig.savefig(self.output, bbox_inches='tight')
+        if(self.PlotFlag):
+            plt.show()  #Uncomment for seeing the plot
         
  
  
