24adcedbfde8005ebb5a604ab298ce3db7462e02
andi klein <pabloemma@gmail.com>
2020-04-25 14:24:56 -0600

    plots and checks for old files

--- src/LCWA_control.py
+++ src/LCWA_control.py
@@ -36,22 +36,35 @@ class MyControl(object):
         
         
         #loop over directories and check if anyone has something older than one week
+        temp = 'LC'
+        dirlist = []
+        for k in range(1,16):
+            if (k<10):
+                temp1 = temp+'0'+str(k)+'_'
+            else:
+                temp1 = temp+str(k)+'_'
+            
+            dirlist.append(temp1)
+            
+        for k in range(len(dirlist)):
+            temp = '/LCWA/'+dirlist[k] # file on dropbox
+            print('now checking ',temp)
+
         
+            MyDir = self.PA.dbx.files_list_folder(temp,recursive=True)
         
-        MyDir = self.PA.dbx.files_list_folder('/LCWA/ROTW',recursive=True)
-        
-        for item in MyDir.entries:
-            if isinstance(item, dropbox.files.FileMetadata):
-                now = datetime.datetime.now() #determine how old a file is
-                diff = now - item.server_modified #take the difference
-                if(diff.days > 8 ):
-                    print ('name = ' , item.name)
-                    print ('path  = ', item.path_display )
-                    print ('fileID = ' , item.id)
-                    print ('date = ', item.server_modified)
+            for item in MyDir.entries:
+                if isinstance(item, dropbox.files.FileMetadata):
+                    now = datetime.datetime.now() #determine how old a file is
+                    diff = now - item.server_modified #take the difference
+                    if(diff.days > 8 ):
+                        print ('name = ' , item.name)
+                        print ('path  = ', item.path_display )
+                        print ('fileID = ' , item.id)
+                        print ('date = ', item.server_modified)
                     # here we delete the files
-                    print("deleting file ",item.path_display )
-                    self.PA.dbx.files_delete(item.path_display)
+                        print("deleting file ",item.path_display )
+                        self.PA.dbx.files_delete(item.path_display)
                     
                  
     
