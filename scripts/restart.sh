81ba934969f9af0b22f63709bb162f92a3858249
andi klein <pabloemma@gmail.com>
2020-05-11 14:05:26 -0600

    added hoepfullly timestamp

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -7,7 +7,7 @@ then
     echo "Process running ...";
 else
 
-python3 /home/pi/git/speedtest/src/test_speed1_3.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002 &
+python3 /home/pi/git/speedtest/src/test_speed1_3.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002 2>&1 | /home/pi/git/speedtest/scripts/predate.sh > /home/pi/run_speedtest.log &
 
 echo "Starting the process";
 fi;
