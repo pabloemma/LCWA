79e84f2e3b5e439a41e27f5a813524ea78fe8a31
andi klein <pabloemma@gmail.com>
2020-05-14 07:05:18 -0600

    added log file

--- scripts/ManageDropbox.sh
+++ scripts/ManageDropbox.sh
@@ -1,3 +1,3 @@
 echo " plotting figures and cleaning up dropbox"
 
-python3 /home/klein/git/speedtest/src/LCWA_control.py  
+python3 /home/klein/git/speedtest/src/LCWA_control.py  2>&1  > /home/klein/LCWA/control.log
