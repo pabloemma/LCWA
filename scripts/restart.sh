fe6eb8e5af174a68d5c5d936fd5ab4a3bf3265ab
andi klein <pabloemma@gmail.com>
2020-05-17 21:01:59 -0600

    new output redirection

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -7,7 +7,8 @@ then
     echo "Process running ...";
 else
 
-python3 -u /home/pi/git/speedtest/src/test_speed1_3.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002 > /home/pi/run_speedtest_restart.log 2>&1 &
+python3 -u /home/pi/git/speedtest/src/test_speed1_3.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002  > /home/pi/run_speedtes_restart.log 2>> /home/pi/run_speedtest_restart_error.log &
+
 
 echo "Starting the process";
 fi;
