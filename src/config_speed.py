e0c4628b1e6c5d8784b43ff6eba2bcaee60ff547
andi klein <pabloemma@gmail.com>
2022-01-07 16:54:20 -0700

    added more variables to json file, made config_speed to be the system checker

--- src/config_speed.py
+++ src/config_speed.py
@@ -4,6 +4,7 @@ Using json (sigh)"""
 
 import json
 import os
+import platform
 
 
 class MyConfig():
@@ -15,23 +16,54 @@ class MyConfig():
        
         
         # Open config file
-
+        print('Directory Name:     ', os.path.dirname(__file__))
        
 
-        if os.exists(config_file) :
-            self.read_json(config_file)
+        if os.path.exists(config_file) :
+            self.ReadJson(config_file)
         else:
             print(" Config file does not exist, exiting     ", config_file)
 
-        def read_json(file_path):
+    def ReadJson(self,file_path):
 
-            print("reading config file")
-            with open(file_path, "r") as f:
-                return json.load(f)
+        print("reading config file")
+        with open(file_path, "r") as f:
+            myconf = json.load(f)
+
+            self.DecodeVariables(myconf)
+
+    def DecodeVariables(self,jsondict):
+        """decodes the json dictionary in to variables"""
+        self.srcdir = jsondict['directory']['srcdir']
+        self.datadir = jsondict['directory']['datadir']
+
+        #these are depending on the operating system
+        mysystem = platform.system()
+
+        self.timeout = jsondict[mysystem]['timeout']
+        self.speedtest = jsondict[mysystem]['speedtest']
+
+
+
+        print('\n\n ***************** configuration**************\n')
+        print(' We are running on platform ' , mysystem, '\n')
+
+        print('Sourcedir            ',self.srcdir)
+        print('Datadir              ',self.datadir)
+        print('timeout command      ',self.timeout)
+        print('speedtest command    ', self.speedtest)
+
+        print('***************************** end of configuration***************\n\n')
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
 
-    run_dir = speedtest/src/
-
+    conf_dir = '/Users/klein/visual studio/LCWA/config/'
+    config_file = conf_dir + 'test_speed_cfg.json'
     MyC = MyConfig(config_file)
