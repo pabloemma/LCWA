0b20407cd57dd52770c377046da4cb8089fb0372
andi klein <pabloemma@gmail.com>
2022-01-15 10:53:18 -0700

    new run_speedtest and restart.sh scripts

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -8,7 +8,7 @@ then
     echo "Process running ...";
 else
 
-python3 -u /home/pi/git/speedtest/src/test_speed1_3.py -t 10 -d /home/pi/git/speedtest/src/LCWA_d.txt -s 18002  > /home/pi/run_speedtes_restart.log 2>> /home/pi/run_speedtest_restart_error.log &
+python3 -u /home/pi/git/speedtest/src/test_speed1_3.py  > /home/pi/run_speedtes_restart.log 2>> /home/pi/run_speedtest_restart_error.log &
 
 
 echo "Starting the process";
