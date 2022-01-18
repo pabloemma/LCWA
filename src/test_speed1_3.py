5f597367c842ccae61facb41b64f9c694b64127e
andi klein <pabloemma@gmail.com>
2022-01-18 09:16:38 -0700

    more issues with config directory resolved. Error in program, where I hgave the src dir as the location of the confdir. conf_dir is now also a new variable in the config file.

--- src/test_speed1_3.py
+++ src/test_speed1_3.py
@@ -153,6 +153,7 @@ class test_speed1():
         self.runmode = MyConfig.runmode         #ookla or iperf
         self.Debug = MyConfig.debug
         self.cryptofile = MyConfig.cryptofile
+        self.conf_dir = MyConfig.conf_dir
 
   
         if (self.runmode == 'Iperf'):
@@ -378,7 +379,7 @@ class test_speed1():
         if(args.dpfile != None):
             self.cryptofile = args.dpfile
             if(self.cryptofile[0] == 'L'): # need to add the system path
-                self.cryptofile = self.speedtest_srcdir + self.cryptofile
+                self.cryptofile = self.speedtest_confdir + self.cryptofile
  
             #self.DropFlag = True
             self.ConnectDropBox() # establish the contact to dropbox
@@ -388,7 +389,7 @@ class test_speed1():
             self.Logging('You need to provide path for cryptofile, will not connect to dropbox')
 
         if(self.cryptofile[0] == 'L'): # need to add the system path
-            self.cryptofile = self.speedtest_srcdir + self.cryptofile
+            self.cryptofile = self.speedtest_confdir + self.cryptofile
             self.ConnectDropBox() # establish the contact to dropbox
         else:
             self.ConnectDropBox()
