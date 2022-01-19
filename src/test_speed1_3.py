cf3b28a9faa595c7a247ccdfbf3f1bf9d2a90ec0
andi klein <pabloemma@gmail.com>
2022-01-19 14:35:32 -0700

    updated version statement

--- src/test_speed1_3.py
+++ src/test_speed1_3.py
@@ -250,7 +250,7 @@ class test_speed1():
         """
         keep track of the updates
         """
-        self.vs = '7.01.06'
+        self.vs = '7.02.01'
  
         
         print(' History')
