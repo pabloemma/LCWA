86955a3beca5b8566b0cba42ae46914a3545a6d8
andi Klein <pabloemma@gmail.com>
2022-01-25 15:30:12 -0700

    Created new version PlotClass1.py, which plots both iperf and speedtest in same plot

--- src/PlotClass1.py
+++ src/PlotClass1.py
@@ -82,77 +82,31 @@ class MyPlot(object):
         
         
         
-        self.temp_name = self.path+'/temp.txt'
-        self.temp_file = open(self.temp_name,'w')
 
     # read csv file into panda data dataframe
         temp_data = pd.read_csv(self.InputFile)
      # now we drop some of the columns, pay attention to white space
-        drop_list =[' server id','jitter','package ',' latency measured']
+        drop_list =['server id','jitter','package','latency measured']
         
         lcwa_data = temp_data.drop(columns = drop_list)
+        # convert date and time back to datetime
+        lcwa_data["Time"] = pd.to_datetime(lcwa_data['time']) 
 
     # Create an iper and a speedtest frame
 
         iperf_opt = [' iperf3']
-        lcwa_iperf = lcwa_data[lcwa_data['server name'].isin(iperf_opt)]  #all the iperf values
-        lcwa_speed = lcwa_data[~lcwa_data['server name'].isin(iperf_opt)]  #all the not iperf values        
+        self.lcwa_iperf = lcwa_data[lcwa_data['server name'].isin(iperf_opt)]  #all the iperf values
+        self.lcwa_speed = lcwa_data[~lcwa_data['server name'].isin(iperf_opt)]  #all the not iperf values        
+        
 
-        self.temp_file.close()
-        
-    def ReadTestData(self,legend):
-        """
-        Reads the results with Matplotlib
-        """
-        
-        self.ReadFile()
-#        self.temp_file.seek(0)
-        #f=open(self.temp_name,'rb')
-        #f=open('/Users/klein/speedfiles/nuke_2020-04-17speedfile.csv') 
-        
-        self.legend = legend #legend is a dictionary'
-        
-        if(self.MyPythonVersion):
-           
-            x1,y1,y2 = np.loadtxt(self.temp_name, delimiter=',',
-                   unpack=True,usecols=(1,7,8),
-                   converters={ 1: self.MyTime},skiprows = 1)
-            
-        else:
-          
-                  
-            x1,y1,y2 = np.loadtxt(self.temp_name, delimiter=',',
-                   unpack=True,usecols=(1,7,8),
-                   converters={ 1: md.strpdate2num('%H:%M:%S')},skiprows=1)
-        self.x1 = x1
-        self.y1 = y1
-        self.y2 = y2
-        self.PlotTestData(x1, y1, y2)
-         
-    
-    def PlotTestData(self,x1,y1,y2):
-        """
-        Plots the tests
-        """
-        np.set_printoptions(precision=2)
-        fig=plt.figure() 
+
+    def PlotData(self):
+        """plots the data structure using pandas and matplotlib"""
+
+
+        fig = plt.figure()
         ax=fig.add_subplot(1,1,1)
-        
-        #Add Ip address
-        
-        
-        #ax.text(.1,.36,'Average $\mu$ and Standard deviation $\sigma$',weight='bold',transform=ax.transAxes,fontsize=13)
-        #ax.text(.1,.23,r'$\mu_{up}     = $'+str(np.around(np.mean(y2),2))+' '+'[Mb/s]'+r'   $\sigma_{up} =     $'+str(np.around(np.std(y2),2)),transform=ax.transAxes,fontsize=12)
-        #ax.text(.1,.3,r'$\mu_{down} = $'+str(np.around(np.mean(y1),2))+' '+'[Mb/s]'+r'   $\sigma_{down} = $'+str(np.around(np.std(y1),2)),transform=ax.transAxes,fontsize=12)
-
-        #add legend
-        print(self.legend)
-        ax.text(.05,.95,'MyIP = '+self.DigIP(),weight='bold',transform=ax.transAxes,fontsize=11)
-
-        plt.plot_date(x1,y1,'bs',label='\n blue DOWN ')
-        plt.plot_date(x1,y2,'g^',label=' green UP')
-        #plt.text(1.,1.,r' $\sigma = .1$')
-        plt.grid(True)
+        ax.text(.05,.95,'iperf and ookla on'+' '+self.DigIP(),weight='bold',transform=ax.transAxes,fontsize=11)
 
         ax.xaxis.set_major_locator(md.MinuteLocator(interval=60))
         ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
@@ -160,34 +114,39 @@ class MyPlot(object):
         plt.ylabel('Speed in Mbs')
 
         plt.title('Speedtest LCWA '+self.InputFile)
-    
-        plt.legend(facecolor='ivory',loc="center left",shadow=True, fancybox=True)
-        if(np.around(np.mean(y1),2) > 40.): #starlink
-            plt.ylim(0.,60.) # set yaxis limit
-        elif(np.around(np.mean(y1),2) <= 40. and np.around(np.mean(y1),2) > 21.):
-            plt.ylim(0.,41.) # set yaxis limit
-        elif(np.around(np.mean(y1),2) <= 21. and np.around(np.mean(y1),2) > 12.):
-            plt.ylim(0.,24.) # set yaxis limit
-        elif(np.around(np.mean(y1),2) <= 12. and np.around(np.mean(y1),2) > 7.):
-            plt.ylim(0.,12.) # set yaxis limit
-             # set yaxis limit
-        elif(np.around(np.mean(y1),2) <= 7. ):
-            plt.ylim(0.,7.) # set yaxis limit
 
-        
+
+
+    
+        plt.plot(self.lcwa_iperf["Time"],self.lcwa_iperf["download"],'bs',label='\n iperf blue DOWN ')
+        plt.plot(self.lcwa_iperf["Time"],self.lcwa_iperf["upload"],'g^',label='\n iperf green UP ')
+        plt.plot(self.lcwa_speed["Time"],self.lcwa_speed["download"],'ks',label='\n speedtest black DOWN ')
+        plt.plot(self.lcwa_speed["Time"],self.lcwa_speed["upload"],'r^',label='\n speedtest red UP ')
+        plt.ylim(0,40.)
+        plt.grid(True)
+
         plt.xticks(rotation='vertical')
         plt.tight_layout()
-        print('input file',self.InputFile)
-
+        plt.legend(facecolor='ivory',loc="lower left",shadow=True, fancybox=True,fontsize = 6)
+ 
         print (self.output)
         fig.savefig(self.output, bbox_inches='tight')
-        if(self.PlotFlag):
-            plt.show()  #Uncomment for seeing the plot
+
+    
+        plt.show()
+
+        
+        
+
+
+
 
 
 
     
+
     
+
     def MyTime(self,b):
         """ conversion routine for time to be used in Matplotlib"""
 
@@ -259,5 +218,6 @@ if __name__ == '__main__':
     PlotFlag = True # flag to plot or not on screen
     MP = MyPlot(path,file,token,PlotFlag,runmode = 'Both')
     MP.ReadFile()    #MP.ReadTestData(legend)
+    MP.PlotData()
     #MP.ConnectDropbox()
     #MP.PushFileDropbox('/LCWA/ROTW/')
