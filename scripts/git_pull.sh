df6d05365c33075dfbaa4c15979d228921b970d6
andi Klein <pabloemma@gmail.com>
2022-01-25 20:37:38 -0700

    new git_pull script to help with transition

--- scripts/git_pull.sh
+++ scripts/git_pull.sh
@@ -1,5 +1,21 @@
 #!
 
 cd /home/pi/git/speedtest
-git pull
-cd /home/pi
+echo $HOSTNAME
+
+if [ ''$HOSTNAME'' = ''LC04'' ]; then
+	echo "we are updating"
+	cd ~/git/speedtest
+    echo $PWD
+	git pull
+	git fetch
+	git checkout test5
+	cd /home/pi
+	
+	
+else
+	git pull
+    cd /home/pi
+fi
+
+
