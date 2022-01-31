195a2591412426d139aca808de4abb765f526c32
andi klein <pabloemma@gmail.com>
2022-01-31 07:11:40 -0700

    changed the restart and run_speedtest back, since all machines I could update are now updated.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -8,9 +8,9 @@ then
     echo "Process running ...";
 else
 #temporary if program dies download all the necessary programs
-cd /home/pi/git/speedtest/src
-./update_speedtest
-cd ~
+#cd /home/pi/git/speedtest/src
+#./update_speedtest
+#cd ~
 
 python3 -u /home/pi/git/speedtest/src/test_speed1_3.py  > /home/pi/run_speedtes_restart.log 2>> /home/pi/run_speedtest_restart_error.log &
 
