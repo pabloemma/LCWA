0fb656efc05764493897d8b5c8bd83df41517c40
Andi Klein <pabloemma@gmail.com>
2022-06-04 08:43:04 -0600

    temp changes

--- src/LCWA_control.py
+++ src/LCWA_control.py
@@ -132,7 +132,7 @@ class MyControl(object):
             dirlist.append(temp1)
         token_file = '/git/speedtest/src/LCWA_d.txt'
         #tempdir = 'scratch'
-        #self.PA =PA =PL.PlotAll(token_file,dirlist,filedate = '2022-02-07')
+        #self.PA =PA =PL.PlotAll(token_file,dirlist,filedate = '2022-06-03')
         self.PA =PA =PL.PlotAll(token_file,dirlist)
         PA.ConnectDropbox()
         PA.GetFiles() 
@@ -236,8 +236,8 @@ if __name__ == '__main__':
     start = datetime.time(23, 49)
     end = datetime.time(23,59)
     # for a different date use the line 132
-    #start = datetime.time(7,29)
-    #end = datetime.time(7,40)
+    #start = datetime.time(8,29)
+    #end = datetime.time(8,45)
     if(start<timestamp<=end):
         print (start <= timestamp <= end) # >>> depends on what time it is
     
