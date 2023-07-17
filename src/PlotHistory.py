ac0813197de5971843c82838706d0bdedb89c0cc
andi klein <pabloemma@gmail.com>
2023-07-17 15:26:37 -0600

    changes to json file

--- src/PlotHistory.py
+++ src/PlotHistory.py
@@ -434,8 +434,8 @@ class PlotHistory(object):
 if __name__ == "__main__":  
     config_file =  'PlotHistory.json'
     speed_box = None #creates loop
-    speed_box = 'LC14' # sngle speedbox
-    begin_time="2023-04-28"
+    #speed_box = 'LC17' # sngle speedbox
+    begin_time="2023-05-28"
     #begin_time="month"
     end_time = 'Today'
     #end_time = "2023-02-27"
--- test_progs/test_dropbox.py
+++ test_progs/test_dropbox.py
@@ -172,12 +172,12 @@ if __name__ == '__main__':
     import os.path
     loop_time       = 600 # every loop_time we will read a file and copy it locally, the time is in seconds
     homedir         = os.path.expanduser('~')
-    #tokenfile       = homedir+'/git/LCWA/src/LCWA_d.txt' # the name and path of the dropbox creds
-    tokenfile       = homedir+'/git/LCWA/src/LCWA_a.txt' # the name and path of the dropbox creds
+    tokenfile       = homedir+'/git/LCWA/src/LCWA_d.txt' # the name and path of the dropbox creds
+    #tokenfile       = homedir+'/git/LCWA/src/LCWA_a.txt' # the name and path of the dropbox creds
     #dropbox_dir     = '/LCWA/ALL_LCWA/' # dir on dropbox
     dropbox_dir     = '/LCWA/LC99_/'
     #dropbox_file    = 'LCWA_TOTAL_2023-05-07speedfile.pdf' # name of file
-    dropbox_file    = 'LC99_2023-05-07speedfile.pdf'
+    dropbox_file    = 'LC99_2023-05-07speedfile_test.pdf'
     local_dir       = homedir+'/scratch/'
 
 
