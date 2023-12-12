735f134799df98e7c9c0b1a82476745d69de51e0
andi klein <pabloemma@gmail.com>
2023-12-12 14:44:39 -0700

    helper script for dropbox

---
+++ scripts/lcwa_dec.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+
+TOKEN_FILE='LCWA'
+ENC_KEY=$(head -n 1 $1)
+
+openssl enc -d -base64 -in "/home/pi/git/speedtest/src/${TOKEN_FILE}.enc" -out "/home/pi/git/speedtest/src/${TOKEN_FILE}_a.txt" -k "$ENC_KEY"
+#openssl enc -d -base64 -in "/Users/klein/git/speedtest/src/${TOKEN_FILE}.enc" -out "/Users/klein/git/speedtest/src/${TOKEN_FILE}_a.txt" -k "$ENC_KEY"
+
