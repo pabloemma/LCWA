92d884ddfcbf24e4ca3b78f7803ef495b4e4db67
andi klein <pabloemma@gmail.com>
2023-12-12 13:56:51 -0700

    helper script

---
+++ scripts/lcwa_dec.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+
+TOKEN_FILE='LCWA'
+ENC_KEY=$(head -n 1 $1)
+
+openssl enc -d -base64 -in "/home/pi/git/speedtest/src/${TOKEN_FILE}.enc" -out "/home/pi/git/speedtest/src/${TOKEN_FILE}_a.txt" -k "$ENC_KEY"
+
