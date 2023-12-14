da6fa8c567bf2f27cc2c9ffdec98656c4c4febcb
andi klein <pabloemma@gmail.com>
2023-12-14 15:57:33 -0700

    created a script which wll be run at end of git_pull , to ensure we have pdating the same night

---
+++ scripts/maintenance.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env bash
+
+# this file is run at the end of a git_pull
+# so that any new push to the system beyond code is executed on the same night
+#right now it is empty, but can be changed to adress new issues
+#
+echo "******************** this is the maintenance script ********************"
+
