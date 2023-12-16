e24fd1cfc010a3657ac8282fa8b32f05631cb390
andi klein <pabloemma@gmail.com>
2023-12-15 17:01:10 -0700

    changed path in mail_test, added another line for decode

--- scripts/lcwa_dec.sh
+++ scripts/lcwa_dec.sh
@@ -6,5 +6,6 @@ TOKEN_FILE='LCWA'
 ENC_KEY=$(head -n 1 $1)
 
 openssl enc -d -base64 -in "/home/pi/git/speedtest/src/${TOKEN_FILE}.enc" -out "/home/pi/git/speedtest/src/${TOKEN_FILE}_a.txt" -k "$ENC_KEY"
+#openssl enc -d -base64 -in "/home/klein/git/speedtest/src/${TOKEN_FILE}.enc" -out "/home/klein/git/speedtest/src/${TOKEN_FILE}_a.txt" -k "$ENC_KEY"
 #openssl enc -d -base64 -in "/Users/klein/git/speedtest/src/${TOKEN_FILE}.enc" -out "/Users/klein/git/speedtest/src/${TOKEN_FILE}_a.txt" -k "$ENC_KEY"
 
