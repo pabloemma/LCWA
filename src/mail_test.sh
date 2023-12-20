6c2937dabd85a4139fdd0e8d37771e163bd94be4
andi klein <pabloemma@gmail.com>
2023-12-19 17:30:34 -0700

    giving up on runnin g mutt on OS, Added  debug statement to LCWA_control.

--- src/mail_test.sh
+++ src/mail_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 subject="$2"
 for I in `cat /home/klein/private/LCWA/recipient_list.txt`; do cat $3 | mutt  -a $1 -s $2 -- $I < $3; echo $I; sleep 3; done
+#for I in `cat /Users/klein/private/LCWA/recipient_list_short.txt`; do cat $3 | /usr/local/bin/mutt  -a $1 -s $2 -- $I < $3; echo $I; sleep 3; done
