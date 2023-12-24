7b4bf67dd72edb814515d4b8f93272162b798ee4
andi klein <pabloemma@gmail.com>
2023-12-24 06:34:05 -0700

    cosmetic change, program is running

--- src/LCWA_control.py
+++ src/LCWA_control.py
@@ -280,7 +280,7 @@ class MyControl(object):
 if __name__ == '__main__':
     #create the list
     debug = False
-    report_date = '2023-12-23'
+    #report_date = '2023-12-23'
     report_date = None
     from pathlib import Path
     # next we get current time so that we can calculate how long the program took
