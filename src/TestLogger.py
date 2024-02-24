1e7cd636494a6e8af406e5729c264552b62c6f28
andi klein <pabloemma@gmail.com>
2024-02-24 16:55:45 -0700

    more work on testlogger

--- src/TestLogger.py
+++ src/TestLogger.py
@@ -6,6 +6,7 @@ Feb 2024
 This is a work in progress :)
 
 '''
+import inspect
 
 class TestLogger():
 
@@ -22,11 +23,38 @@ class TestLogger():
 
     """
 
-    def __init__(self,LogFile = None, LogLevel= None, Output = None )
+    def __init__(self,LogFile = None, LogLevel= None, Output = None ):
+
+        TX = color
         
+        
+        if(Output == 'logfile' or Output == 'both'):
+            self.Openfile()
+        else:
+            frame = inspect.currentframe()
+            prefix = TX.BOLD +TX.RED+'|'+frame.f_code.co_name+'>'+TX.END
+            #print('\033[1m'+prefix)
+            #print('test')
 
- 
 
+
+        print(prefix)
+
+    #def OpenFile(self,LogFile)
+
+
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 if __name__ == '__main__':
-    TL = TestLogger(LogFile = ' test.log' , LogLevel = 1 , Output = 'both')
+    TL = TestLogger(LogFile = ' test.log' , LogLevel = 1 , Output = 'screen')
