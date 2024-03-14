4fedae118c3486793c4293d23690704b63d68923
andi klein <pabloemma@gmail.com>
2024-03-14 06:58:02 -0600

    test program for loguru

---
+++ src/test_loguru.py
@@ -0,0 +1,12 @@
+# using the loguru logger
+import sys
+import os
+import json
+from loguru import logger
+
+
+# https://github.com/Delgan/loguru?tab=readme-ov-file
+
+#import my_module
+
+class MyLogger():
