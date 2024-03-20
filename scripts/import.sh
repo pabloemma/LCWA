95a42a5a65359834d17fabad19e450c83a9c151d
andi klein <pabloemma@gmail.com>
2024-03-20 13:24:09 -0600

    added import.sh

---
+++ scripts/import.sh
@@ -0,0 +1,6 @@
+sudo apt install --force-yes python3-matplotlib
+sudo apt install --force-yes python3-dropbox
+sudo apt-install --force-yes  python3-speedtest
+sudo apt-install  --force-yes python3-loguru
+
+
