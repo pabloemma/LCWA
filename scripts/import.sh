a70c2e8f6e75c944fc8b5148f7fc3d76ed48c617
andi klein <pabloemma@gmail.com>
2024-03-26 08:09:57 -0600

    new import script

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,6 +1,14 @@
 sudo apt install --force-yes python3-matplotlib
 sudo apt install --force-yes python3-dropbox
-sudo apt install --force-yes  python3-speedtest
+
 sudo apt install  --force-yes python3-loguru
+sudo apt install  --force-yes python3-pandas
+sudo apt install  --force-yes python3-iperf3
 
 
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+
+sudo apt-get install curl
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+
+sudo apt-get install speedtest
