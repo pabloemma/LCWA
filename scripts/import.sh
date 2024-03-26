3a31fc518a3f0da946ad8358f1a8c3db948faf5b
andi klein <pabloemma@gmail.com>
2024-03-26 09:11:16 -0600

    latest versions of doc and import

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,13 +1,18 @@
+mkdir ~/speedfiles
+
 sudo apt install --force-yes python3-matplotlib
 sudo apt install --force-yes python3-dropbox
 
 sudo apt install  --force-yes python3-loguru
 sudo apt install  --force-yes python3-pandas
-sudo apt install  --force-yes python3-iperf3
+
+sudo apt install  --force-yes python3-ntplib
 
 
 
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+pip3 install tcp_latency
+pip3 install iperf3
 
 curl -O  https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-armel.tgz
 tar -zxf ookla-speedtest-1.2.0-linux-armel.tgz
