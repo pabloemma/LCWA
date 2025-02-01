33a12b6592e47e0b0625a564068a4c0f9f1fe03e
Andi Klein <pabloemma@gmail.com>
2025-02-01 09:52:28 -0700

    update Managedropbox

--- scripts/ManageDropbox.sh
+++ scripts/ManageDropbox.sh
@@ -1,3 +1,3 @@
 echo " plotting figures and cleaning up dropbox"
 
-python3 /home/klein/git/speedtest/src/LCWA_control.py  /home/klein/LCWA/control.log 2>&1
+python3 /home/klein/git/speedtest/src/LCWA_control.py 
